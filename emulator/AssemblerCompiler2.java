7acafc8a3fc99d77db050f2e9943ca96c4af496e
bbjay <bbjay@here.local>
2012-11-03 17:52:53 +0100

    added a preprocessor to the compiler, can resolve labels now

--- emulator/AssemblerCompiler2.java
+++ emulator/AssemblerCompiler2.java
@@ -1,5 +1,6 @@
 package ch.zhaw.inf3.emulator;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class AssemblerCompiler2 extends AssemblerCompiler {
@@ -11,29 +12,71 @@ public class AssemblerCompiler2 extends AssemblerCompiler {
 
 	public short[] compile(List<String> list) {
 		short[] code = new short[list.size()];
+		preprocess(list);
+
 		for (int i = 0; i < list.size(); i++) {
 			String[] subString = list.get(i).split(" ");
 			Instruction instruction = isa.getByMnemonic(subString[0]);
 			
 			short[] operands = new short[2];
 			if (subString.length > 1 ) {
-				operands[0] = parseRegister(subString[1]);
+				operands[0] = parseInt(subString[1]);
 			}
 			if (subString.length > 2 ) {
-				operands[1] = parseAddress(subString[2]);				
+				operands[1] = parseInt(subString[2]);
 			}
 			//System.out.println(operands[0] +" "+ operands[1]);
 			
-			code[i] = instruction.encodeOperandValues(operands);
+			if(instruction != null) {
+				code[i] = instruction.encodeOperandValues(operands);
+			}
+			else{ // also allow raw numeric values
+				code[i] = (short)Integer.parseInt(list.get(i));
+			}
 		}
 		return code;
 	}
 	
-	public short parseRegister(String reg){
-		return (short)Integer.parseInt(reg.substring(1, 2));
+	public short parseInt(String reg){
+		String num = reg.replaceFirst("R", "").replaceFirst("#", "");
+		return (short)Integer.parseInt(num);
 	}
 	
-	public short parseAddress(String addr){
-		return (short)Integer.parseInt(addr.substring(1, addr.length()));
+	/**
+	 * @param list
+	 * @return list
+	 * Preprocesses the code to handle it over to the compiler.
+	 * Resolves labels to absolute addresses
+	 */
+	private List<String> preprocess(List<String> list){
+		HashMap<String,Integer> labelLookUpTable = new HashMap<String, Integer>();
+		// search for label declarations, store its address in the LUT and remove it from our code
+		for (int i = 0; i < list.size(); i++) {
+			String line = list.get(i);
+			if (line.endsWith(":")) {
+				String label = list.get(i).replaceFirst(":", "");
+				labelLookUpTable.put(label, i);
+				list.remove(i);
+			}
+		}
+		// search for label usage and replace it with corresponding address
+		for (String line : list) {
+			String[] part = line.split(" ");
+			boolean found = false;
+			for (int i = 1; i < part.length; i++) {
+				Integer relative_addr = labelLookUpTable.get(part[i]);
+				if(relative_addr != null){
+					part[i] = "#"+ Integer.valueOf(relative_addr + isa.start_offset).toString(); // replace label with resolved address
+					found = true;
+				}
+			}
+			if (found) {
+				String comp = "";
+				for(String s: part) comp += s +" " ;
+				int index = list.indexOf(line);
+				list.set(index, comp); // replace line
+			}
+		}
+		return list;
 	}
 }
